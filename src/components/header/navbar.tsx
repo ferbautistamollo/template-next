298d3addeee2f3db7d49dd79acf2d1f31185472c
ferbautistamollo <ferbautistamollo@gmail.com>
2025-07-22 09:37:51 -0400

    ACTUALIZACIÓN DE NAVBAR

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -4,35 +4,57 @@ import {
   NavbarItem,
   Navbar as NextUINavbar,
 } from "@heroui/navbar";
-import NextLink from "next/link";
-import { Suspense } from "react";
+import { Link } from "@heroui/link";
+import React, { Suspense } from "react";
 
+import { getDeployEnvironment } from "@/utils/env";
 import { Logo } from "@/components/common/icons";
 import { ThemeSwitch } from "@/components/common/theme-switch";
 import UserComponent from "@/components/header/user";
 import { urlLogin } from "@/utils/services";
 import { getUserCookie } from "@/utils/helpers/cookie";
-export const Navbar = async () => {
+
+interface Props {
+  name?: String;
+}
+export const Navbar = async ({ name }: Props) => {
   const { data } = await getUserCookie();
+  const environment = getDeployEnvironment();
 
   return (
-    <Suspense
-      fallback={
-        <div className="flex justify-center items-center h-screen">
-          Loading...
-        </div>
-      }
-    >
-      <NextUINavbar maxWidth="xl" position="sticky">
-        <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-          <NavbarBrand as="li" className="gap-3 max-w-fit">
-            <NextLink
-              className="flex justify-start items-center gap-1"
-              href={`${urlLogin}/apphub`}
-            >
-              <Logo height={30} width={80} />
-            </NextLink>
-          </NavbarBrand>
+    <Suspense fallback={<div>Cargando...</div>}>
+      <NextUINavbar
+        isBordered
+        className="border-r light:border-gray-200 dark:border-gray-700"
+        maxWidth="full"
+        position="sticky"
+      >
+        <NavbarBrand>
+          <Link
+            className="flex justify-start items-center gap-1"
+            href={`${urlLogin}/apphub`}
+          >
+            <Logo height={30} width={80} />
+          </Link>
+        </NavbarBrand>
+
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          <NavbarContent className="hidden sm:flex gap-4" justify="center">
+            <NavbarItem>
+              <div className="flex flex-col items-center text-center leading-tight">
+                <span className="font-bold text-md uppercase">
+                  {name ?? "NOMBRE HERRAMIENTA"}
+                </span>
+                {(environment === "dev" || environment === "test") && (
+                  <span className="mt-1 text-xs font-medium text-white bg-red-500 px-2 py-0.5 rounded-sm shadow-xs shadow-red-300 border border-white/20">
+                    {environment === "test"
+                      ? "VERSIÓN DE PRUEBAS"
+                      : "VERSIÓN DE DESARROLLO"}
+                  </span>
+                )}
+              </div>
+            </NavbarItem>
+          </NavbarContent>
         </NavbarContent>
 
         <NavbarContent
