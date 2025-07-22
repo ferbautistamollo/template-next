553efe5923a0e35b0dcf9c1f123b7fb916851571
ferbautistamollo <ferbautistamollo@gmail.com>
2025-07-22 09:37:33 -0400

    CREACIÓN DE ARCHIVO DE TURRIL

---
+++ src/components/common/index.tsx
@@ -0,0 +1,13 @@
+export {
+  MuserpolLogo,
+  Logo,
+  SearchIcon,
+  MoonFilledIcon,
+  SunFilledIcon,
+  CollapseFalseIcon,
+  CollapseTrueIcon,
+  HomeIcon,
+} from "./icons";
+
+export { BreadcrumbsState } from "./breadcrumbsState";
+export { subtitle } from "./primitives";
